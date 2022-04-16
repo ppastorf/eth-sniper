9fd665cea848adfbe82bc4b06cf6d44271c341bd
pedro.fernandes <pedro.fernandes@ifood.com.br>
2022-04-15 21:38:51 -0300

    bsc contract abi generation script update

--- scripts/gen-contract-bindings-bsc.sh
+++ scripts/gen-contract-bindings-bsc.sh
@@ -2,17 +2,20 @@
 
 set -euo pipefail
 
-name=$1
-address=$2
+dex_name=$1
+contract_name=$2
+address=$3
 
-pgk_name="$(echo $name | awk '{print tolower($0)}')"
-abi_file="contracts/bsc/${pkg_name}/${name}.abi"
-go_file="contracts/bsc/${pkg_name}/${name}.go"
+pkg_name=$(echo $dex_name | awk '{print tolower($0)}')
+abi_file="contracts/bsc/${pkg_name}/${contract_name}.abi"
+go_file="contracts/bsc/${pkg_name}/${contract_name}.go"
 
-mkdir -p "contracts/bsc/${name}"
+mkdir -p "contracts/bsc/${pkg_name}"
 
 curl -s -X GET "https://api.bscscan.com/api?module=contract&action=getabi&address=${address}" \
     | jq .result | sed -e 's/\\//g' -e 's/^"//g' -e 's/"$//g' \
     | jq > "$abi_file"
 
-abigen --abi "$abi_file" --out "$go_file" --pkg "$pkg_name"
+abigen --abi "$abi_file" --out "$go_file" --pkg "$contract_name"
+
+sed -i'' "s/package ${contract_name}$/package ${pkg_name}/g" $go_file
